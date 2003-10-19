ef58c81d3c23b8fb2921feb9e234f0859402636c
allenb <allenb@43ae9f96-ec1a-11de-8de6-035db03795fd>
2003-10-19 22:51:24 +0000

    Refactor boost options to make more flexible and add python support.
    This change "should" be backwards compatible, but I am not making any promises. :)
    
    This change gets us started down the path of making the boost options object work for a variety of boost libraries.
    
    It introduces:
    - More complete checking wether a library exists (test linking against it)
    - Provides for cannonical name detection
    - Support for boost.python module creation environment update method
    - other various things...

--- trunk/scons-addons/src/SConsAddons/Options/Boost.py
+++ trunk/scons-addons/src/SConsAddons/Options/Boost.py
@@ -11,8 +11,9 @@ __revision__ = "__FILE__ __REVISION__ __DATE__ __DEVELOPER__"
 import SCons.Environment
 import SCons
 import SConsAddons.Options
-from SConsAddons.Options import LocalUpdateOption
+import SConsAddons.Util
 import SCons.Util
+import distutils.sysconfig
 
 import string
 import sys
@@ -39,54 +40,85 @@ class Boost(SConsAddons.Options.PackageOption):
       help_text = """Base directory for Boost. include, and lib should be under this directory"""
       self.baseDirKey = "BoostBaseDir"
       self.requiredVersion = requiredVersion
-      self.libs = map(lambda x: 'boost_'+x, libs)
+      self.lib_names = libs
       self.required = required
-      LocalUpdateOption.__init__(self, name, self.baseDirKey, help_text)
+      SConsAddons.Options.PackageOption.__init__(self, name, self.baseDirKey, help_text)
+      self.available = False
 
       # configurable options
       self.baseDir = None
-      self.boostHeader = None
+      self.setupLibrarySettings()
 
+   def setupLibrarySettings(self):
+      # Map from non-standard library names to the standard equivalent
+      self.libnameMap = { 'python_debug':'python',
+                          'signalsd':'signals',
+                          'threadd':'thread' }
+
+      # Map from library name to header to check for       
+      self.headerMap = { 'python':'boost/python.hpp',
+                         'thread':'boost/thread.hpp',
+                         'filesystem':'boost/filesystem/operations.hpp' }
+      
+      # Map for extra libs needed for config test
+      self.extraEnvOptions = {}
+      
+      # distutils.sysconfig.get_config_vars()
+      self.python_inc_dir = distutils.sysconfig.get_python_inc()
+      #python_link_share_flags = distutils.sysconfig.get_config_var('LINKFORSHARED')
+      self.python_link_share_flags = "-Wl,-export-dynamic"
+      self.python_lib_path = "/usr/lib/python2.2/config"
+      self.python_extra_libs = ["python2.2", "util", "pthread"]
+      
+      
+   def canonName(self, libname):
+      """ Returns the canonical name of a library """
+      if self.libnameMap.has_key(libname):
+         return self.libnameMap[libname]
+      else:
+         return libname
+      
+      
    def checkRequired(self, msg):
       """ Called when there is config problem.  If required, then exit with error message """
       print msg
       if self.required:
-         SCons.Script.SConscript.Exit()
+         sys.exit(0)
 
    def isAvailable(self):
       return self.available
 
    def setInitial(self, optDict):
       " Set initial values from given dict "
-      sys.stdout.write("checking for Boost...")
+      print "loading initial settings for boost"
       if optDict.has_key(self.baseDirKey):
          self.baseDir = optDict[self.baseDirKey]
-         self.boostHeader = pj(self.baseDir, 'include', 'boost', 'version.hpp')
-         sys.stdout.write("specified or cached. [%s].\n"% self.baseDir)
-         assert os.path.isfile(self.boostHeader), "boost/version.hpp does not exist"
+         print "   %s specified or cached. [%s]."% (self.baseDirKey, self.baseDir)
 
    def find(self, env):
       # Quick exit if nothing to find
       if self.baseDir != None:
          return
-
+      
+      ver_header = None
 
       # Find boost/version.hpp
-      sys.stdout.write("searching...\n")
-      self.boostHeader = SCons.Script.SConscript.FindFile(pj('boost', 'version.hpp'),
+      print "   searching for boost..."
+      if env.Dictionary().has_key('CPPPATH'):
+         ver_header = env.FindFile(pj('boost','version.hpp'), env['CPPPATH'])
+         
+      if env.Dictionary().has_key('CPLUS_INCLUDE_PATH'):
+         ver_header = SCons.Script.SConscript.FindFile(pj('boost', 'version.hpp'),
                                     string.split(env['ENV']['CPLUS_INCLUDE_PATH'], os.pathsep))
-      if None == self.boostHeader:
+         
+      if None == ver_header:
          self.checkRequired("   could not find boost/version.hpp.")
       else:
-         self.boostHeader = str(self.boostHeader)
-         sys.stdout.write("   found boost/version.hpp.\n")
-         ver_file = file(self.boostHeader)
-         found_ver_str = int(re.search("define\s+?BOOST_VERSION\s+?(\d*)", ver_file.read()).group(1))
-         found_ver_str = str(found_ver_str / 100000) + '.' + str(found_ver_str / 100 % 1000) + '.' + str(found_ver_str % 100)
-         sys.stdout.write("   version:%s"%found_ver_str)
+         ver_header = str(ver_header)
+         print "   found boost/version.hpp.\n"
 
          # find base dir
-         self.baseDir = os.path.dirname(os.path.dirname(os.path.dirname(self.boostHeader)))
+         self.baseDir = os.path.dirname(os.path.dirname(os.path.dirname(ver_header)))
          if not os.path.isdir(self.baseDir):
             self.checkRequired("   returned directory does not exist:%s"% self.baseDir)
             self.baseDir = None
@@ -102,64 +134,117 @@ class Boost(SConsAddons.Options.PackageOption):
 
    def validate(self, env):
       # Check that path exist
-      # Check that an include file: boost/config.hpp  exists
-      # Update the temps for later usage
+      # Check that an include file: boost/version.hpp  exists
       passed = True
-      if not os.path.isdir(self.baseDir):
+      
+      if not self.baseDir:
          passed = False
-         self.checkRequired("boost base dir does not exist:%s"%self.baseDir)
-      if not os.path.isfile(self.boostHeader):
+         self.checkRequired("Boost base dir not set")
+         return
+      
+      if not os.path.isdir(self.baseDir):    # If we don't have a directory
          passed = False
-         self.checkRequired("boost/thread.hpp does not exist:%s"%self.boostHeader)
-      for lib in self.libs:
-         static_lib = env.subst('${LIBPREFIX}%s${LIBSUFFIX}' % (lib))
-         shared_lib = env.subst('${SHLIBPREFIX}%s${SHLIBSUFFIX}' % (lib))
-         libdir = pj(self.baseDir, 'lib')
-         if not os.path.isfile(pj(libdir, static_lib)) and not os.path.isfile(pj(libdir, shared_lib)):
-            passed = False
-            self.checkRequired('required library does not exist: %s' % (lib))
+         self.checkRequired("Boost base dir is not a directory: %s" % self.baseDir)
+         return
 
-      # Check version requirement
-      ver_file = file(self.boostHeader)
+      version_header = pj(self.baseDir, 'include', 'boost', 'version.hpp')         
+      if not os.path.isfile(version_header):
+         passed = False
+         self.checkRequired("Boost version.hpp header does not exist:%s"%version_header)
+
+      # --- Check version requirement --- #
+      ver_file = file(version_header)
       found_ver_str = int(re.search("define\s+?BOOST_VERSION\s+?(\d*)", ver_file.read()).group(1))
       found_ver_str = str(found_ver_str / 100000) + '.' + str(found_ver_str / 100 % 1000) + '.' + str(found_ver_str % 100)
       req_ver = [int(n) for n in self.requiredVersion.split('.')]
       found_ver = [int(n) for n in found_ver_str.split('.')]
+      print "   boost version:", ".".join([str(x) for x in found_ver])
       if found_ver < req_ver:
          passed = False
          self.checkRequired("   found version is to old: required:%s found:%s"%(self.requiredVersion,found_ver_str))
 
-      self.found_incs = None
-      self.found_libs = None
-      self.found_lib_paths = None
-      self.found_link_from_libs = None
+      # Returns lists of the options we want
+      self.found_incs = [pj(self.baseDir, 'include')]
+      self.found_lib_paths = [pj(self.baseDir, 'lib')]
 
+      ######## BUILD CHECKS ###########  
+      # --- Check building against libraries --- #   
+
+      # For each library, find cononical lib name and associated header to check
+      # default to checking lib with config.hpp
+      for libname in self.lib_names:
+         canonical_libname = self.canonName(libname)
+         header_to_check = 'boost/config.hpp'
+         if self.headerMap.has_key(canonical_libname):
+            header_to_check = self.headerMap[canonical_libname]
+
+         # Create config environment
+         conf_env = env.Copy()
+         conf_env.Append(CPPPATH = self.found_incs, LIBPATH = self.found_lib_paths)
+         if "python" == canonical_libname:
+            conf_env.Append(CPPPATH = self.python_inc_dir,
+                            LIBPATH = self.python_lib_path,
+                            LIBS = self.python_extra_libs + ["dl",])
+            
+         conf_ctxt = Configure(conf_env)            
+         if not conf_ctxt.CheckLibWithHeader("boost_" + libname, header_to_check, "c++"):
+            passed = False
+            self.checkRequired("Can't compile test program: lib: %s header:%s"%(libname,header_to_check))
+            
+         conf_ctxt.Finish()
+
+      # --- Handle final settings ---- #     
       if not passed:
          # Clear everything
          self.baseDir = None
-         self.boostHeader= None
          edict = env.Dictionary()
          if edict.has_key(self.baseDirKey):
             del edict[self.baseDirKey]
+         self.found_incs = None
+         self.found_lib_paths = None
       else:
-         # Returns lists of the options we want
-         self.found_incs = [pj(self.baseDir, 'include')]
-         self.found_libs = self.libs
-         self.found_lib_paths = [pj(self.baseDir, 'lib')]
          self.available = True
 
-   def updateEnv(self, env):
-      """ Add environment options for building against Boost.Thread"""
+         
+   def updateEnv(self, env, libs=None):
+      """ Add environment options for building against Boost libraries """
       if self.found_incs:
          env.Append(CPPPATH = self.found_incs)
-      if self.found_libs:
-         env.Append(LIBS = self.found_libs)
       if self.found_lib_paths:
          env.Append(LIBPATH = self.found_lib_paths)
+      for l in self.lib_names:
+         if 'python' != l:               # Don't add python by default
+            env.Append(LIBS = ["boost_" + l])
+            
+   def updatePythonModEnv(self, env):
+      """ Update the environment for building python modules """
+      if not "python" in [self.canonName(l) for l in self.lib_names]:
+         print "Tried to updatePythonModEnv with boost option object not configured with python library.\n"
+         sys.exit(0)
+         
+      if self.found_incs:
+         env.Append(CPPPATH = self.found_incs)
+      if self.found_lib_paths:
+         env.Append(LIBPATH = self.found_lib_paths)
+      
+      env.Append(CPPPATH = [self.python_inc_dir,],
+                 LINKFLAGS = self.python_link_share_flags,
+                 LIBPATH = self.python_lib_path,
+                 LIBS = self.python_extra_libs)
+      env["SHLIBPREFIX"] = ""              # Clear the library prefix settings
+      if(SConsAddons.Util.GetPlatform() == "linux"):
+         env['CXXCOM'] += " ; objcopy --set-section-flags .debug_str=contents,debug $TARGET"
+         env['SHCXXCOM'] += " ; objcopy -v --set-section-flags .debug_str=contents,debug $TARGET $TARGET"
+
 
    def dumpSettings(self):
-      "WRite out the settings"
+      "Write out the settings"
       print "BoostBaseDir:", self.baseDir
       print "CPPPATH:", self.found_incs
-      print "LIBS:", self.found_libs
+      print "LIBS:", self.lib_names
       print "LIBPATH:", self.found_lib_paths
+      print "Python settings"
+      print "   inc:", self.python_inc_dir
+      print "   link:", self.python_link_share_flags
+      print "   lib:", self.python_extra_libs
+      print "   lib path:", self.python_lib_path
