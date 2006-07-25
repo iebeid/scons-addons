16c92b493aa5d2cfccb157a66c7a66aca9cd678c
allenb <allenb@43ae9f96-ec1a-11de-8de6-035db03795fd>
2006-07-25 20:58:40 +0000

    Add autolink support for win32.

--- trunk/scons-addons/src/SConsAddons/Options/CppDom.py
+++ trunk/scons-addons/src/SConsAddons/Options/CppDom.py
@@ -30,10 +30,7 @@ import SCons;
 import SCons.Util
 import SConsAddons.Options;   # Get the modular options stuff
 import SConsAddons.Util
-import sys;
-import os;
-import re;
-import string;
+import sys, os, re, string
 
 from SCons.Util import WhereIs
 pj = os.path.join;
@@ -43,17 +40,22 @@ Configure = SCons.SConf.SConf     # Use same alias as SConsctruct sees
 
 
 class CppDom(SConsAddons.Options.PackageOption):
-   def __init__(self, name, requiredVersion, required=True):
+   def __init__(self, name, requiredVersion, required=True, 
+                autoLink=True, preferDynamic=True):
       """
          name - The name to use for this option
          requiredVersion - The version of cppdom required (ex: "0.1.0")
          required - Is the dependency required?  (if so we exit on errors)
+         autoLink - Attempt to use autolinking on windows (default: True)
+         preferDynamic - If true, prefer to use shared libraries.
       """
       help_text = """Base directory for cppdom. bin, include, and lib should be under this directory""";
       self.baseDirKey = "CppDomBaseDir";
       self.requiredVersion = requiredVersion;
       self.required = required;
       self.available = False
+      self.autoLink = autoLink
+      self.preferDynamic = preferDynamic      
       SConsAddons.Options.LocalUpdateOption.__init__(self, name, self.baseDirKey, help_text);
       
       # configurable options
@@ -95,10 +97,7 @@ class CppDom(SConsAddons.Options.PackageOption):
       if None == self.cppdomconfig_cmd:
          self.checkRequired("   could not find cppdom-config.");
       else:
-         sys.stdout.write("   found cppdom-config.\n");
-         found_ver_str = os.popen(self.cppdomconfig_cmd + " --version").read().strip();
-         sys.stdout.write("   version:%s"%found_ver_str);
-         
+         sys.stdout.write("   found cppdom-config: %s"%self.cppdomconfig_cmd);
          # find base dir
          self.baseDir = os.popen(self.cppdomconfig_cmd + " --prefix").read().strip();
          if not os.path.isdir(self.baseDir):
@@ -122,32 +121,62 @@ class CppDom(SConsAddons.Options.PackageOption):
       if not has_config_cmd:
          print "Can not find %s. Limping along without it."%self.cppdomconfig_cmd
       else:
-         cfg_cmd_parser = SConsAddons.Util.ConfigCmdParser(self.cppdomconfig_cmd)
+         cfg_cmd_parser = SConsAddons.Util.ConfigCmdParser(self.cppdomconfig_cmd)           
       
-      if has_config_cmd:
-         # Check version requirement
-         found_ver_str = cfg_cmd_parser.getVersion()
-         req_ver = [int(n) for n in self.requiredVersion.split(".")];
-         found_ver = [int(n) for n in found_ver_str.split(".")];
-         if found_ver < req_ver:
-            passed = False;
-            self.checkRequired("   found version is to old: required:%s found:%s"%(self.requiredVersion,found_ver_str));             
-      
-      # Check header file
-      cppdom_header_file = pj(self.baseDir, 'include', 'cppdom', 'cppdom.h');
-      if not os.path.isfile(cppdom_header_file):
-         passed = False;
-         self.checkRequired("cppdom.h not found:%s"%cppdom_header_file);
+      # -- Find header directory -- #
+      inc_dir = None
+      header_file = pj('cppdom', 'cppdom.h')
+      base_include = pj(self.baseDir,'include')
+      # check standard directory first
+      if os.path.isfile(pj(base_include, header_file)):
+         inc_dir = base_include
+      # check versioned directories by building a list and sorting them
+      elif os.path.exists(base_include):
+         pot_dirs = [pj(base_include,d) for d in os.listdir(base_include)\
+                                              if d.count("cppdom")]
+         pot_dirs.sort()
+         pot_dirs.reverse()         
+         for d in pot_dirs:
+            if os.path.isfile(pj(d,header_file)):
+               inc_dir = d
+               break
+         
+      if not inc_dir:
+         passed = False
+         self.checkRequired("cppdom.h not found in any cppdom directories.")
+      else:
+         print "   Found cppdom include directory: ", inc_dir
 
+      # --- Check version requirement --- #
+      req_ver = [int(n) for n in self.requiredVersion.split(".")];
+      if has_config_cmd:         
+         found_ver_str = cfg_cmd_parser.getVersion()         
+         found_ver = [int(n) for n in found_ver_str.split(".")];         
+      else:
+         found_ver = GetCppDomVersion(inc_dir)
+         found_ver_str = '.'.join([str(i) for i in found_ver])
+      
+      print "   Found cppdom version: ", found_ver_str
+      if found_ver < req_ver:
+         passed = False
+         self.checkRequired("   found version is to old: required:%s found:%s"%(self.requiredVersion,found_ver_str))
+      lib_ver_str = '_'.join([str(i) for i in found_ver])         
+      
+      # --- Build flag settings --- #         
       if has_config_cmd:
          # Returns lists of the options we want
-         self.found_incs = cfg_cmd_parser.findIncludes(" --cxxflags")
+         self.found_incs = inc_dir + cfg_cmd_parser.findIncludes(" --cxxflags")
          self.found_libs = cfg_cmd_parser.findLibs()
          self.found_lib_paths = cfg_cmd_parser.findLibPaths()
       else:
-         self.found_incs = [pj(self.baseDir,'include'),]
-         self.found_libs = ['cppdom',]
+         self.found_incs = [inc_dir,]         
          self.found_lib_paths = [pj(self.baseDir,'lib'),]
+         # On win32 we go with autolinking
+         if SConsAddons.Util.GetPlatform() == "win32" and self.autoLink:
+            self.found_libs = []
+         else:
+            #assert False, "cppdom options doesn't currently know how to get correct version without cppdom-config"
+            self.found_libs = ['cppdom-%s'%lib_ver_str]         
       
       # Try to build against the library
       conf_env = env.Copy();                     # Make a copy of the env
@@ -181,19 +210,34 @@ class CppDom(SConsAddons.Options.PackageOption):
    def apply(self, env):
       """ Add environment options for building against vapor"""
       if self.found_incs:
-         env.Append(CPPPATH = self.found_incs);
+         env.AppendUnique(CPPPATH = self.found_incs);
       if self.found_libs:
-         env.Append(LIBS = self.found_libs);
+         env.AppendUnique(LIBS = self.found_libs);
       if self.found_lib_paths:
-         env.Append(LIBPATH = self.found_lib_paths);
+         env.AppendUnique(LIBPATH = self.found_lib_paths);
+      if self.autoLink and SConsAddons.Util.GetPlatform() == "win32":
+         env.AppendUnique(CPPDEFINES = ['CPPDOM_AUTO_LINK'])
+         if self.preferDynamic:
+            env.AppendUnique(CPPDEFINES = ['CPPDOM_DYN_LINK'])
    
    def getSettings(self):
       return [(self.baseDirKey, self.baseDir),]
 
    def dumpSettings(self):
       "Write out the settings"
-      print "CppDomBaseDir:", self.baseDir;
-      print "cppdom-config:", self.cppdomconfig_cmd;
-      print "CPPPATH:", self.found_incs;
-      print "LIBS:", self.found_libs;
-      print "LIBPATH:", self.found_lib_paths;
+      print "CppDomBaseDir:", self.baseDir
+      print "cppdom-config:", self.cppdomconfig_cmd
+      print "CPPPATH:", self.found_incs
+      print "LIBS:", self.found_libs
+      print "LIBPATH:", self.found_lib_paths
+      
+def GetCppDomVersion(incPath):
+   """Gets the CppDom version from cppdom/version.h.
+      Returns version as tuple (major,minor,patch)
+   """
+   contents = open(pj(incPath,'cppdom','version.h'), 'r').read()
+   major = re.compile('.*(#define *CPPDOM_VERSION_MAJOR *(\d+)).*', re.DOTALL).sub(r'\2', contents)
+   minor = re.compile('.*(#define *CPPDOM_VERSION_MINOR *(\d+)).*', re.DOTALL).sub(r'\2', contents)
+   patch = re.compile('.*(#define *CPPDOM_VERSION_PATCH *(\d+)).*', re.DOTALL).sub(r'\2', contents)
+   return (int(major), int(minor), int(patch))
+      
