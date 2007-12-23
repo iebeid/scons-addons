ec73e6c92d6405ddc9c1f4a024e401928bc3a30c
dshipton <dshipton@43ae9f96-ec1a-11de-8de6-035db03795fd>
2007-12-23 01:15:29 +0000

    Handle frameworks coming from flagpoll

--- trunk/scons-addons/src/SConsAddons/Options/FlagPollBasedOption.py
+++ trunk/scons-addons/src/SConsAddons/Options/FlagPollBasedOption.py
@@ -145,7 +145,8 @@ class FlagPollBasedOption(SConsAddons.Options.PackageOption):
          self.found_cxxflags       = self.flagpoll_parser.findCXXFlags()
          self.found_libs           = self.flagpoll_parser.findLibs()
          self.found_lib_paths      = self.flagpoll_parser.findLibPaths()
-         self.found_link_from_libs = self.flagpoll_parser.findLinkFlags()
+         self.found_link_from_libs = self.flagpoll_parser.findLinkFlags() +
+                                     self.flagpoll_parser.findFrameworks()
 
       #print "-----------------------"
       #print "self.found_libs:", self.found_libs
